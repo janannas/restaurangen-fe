1992d358ff8937903fbb867bb677f52fb6839178
Ulrica Bonnerstig <u.bonnerstig@gmail.com>
2019-09-04 12:38:00 +0200

    Add working api-call for getting free tables

--- src/utils/ApiCalls.tsx
+++ src/utils/ApiCalls.tsx
@@ -11,8 +11,20 @@ class ApiCalls {
     return axios.get("http://localhost/bookings/get-configuration.php");
   }
 
-  fetchBookedTables() {
-    return axios.get("http://localhost/bookings/get-booked-tables.php");
+  fetchBookedTables(date: string) {		
+		axios.post('http://localhost/bookings/get-booked-tables.php/', {
+			date: date
+		}, {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+			}
+		})
+		.then(response => {
+			console.log(response)
+		})
+		.catch(error => {
+			console.log(error.response)
+		});
   }
 
   getAllBookings() {
