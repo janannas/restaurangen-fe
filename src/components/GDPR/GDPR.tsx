956afd90d3c376ebdd11911aa72b0ed276d4149a
Johanna <josjlin@gmail.com>
2019-09-05 13:49:55 +0200

    add GDPR functionality

--- src/components/GDPR/GDPR.tsx
+++ src/components/GDPR/GDPR.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
 
-interface IProps {
-  msg: string;
+interface IGDPRProps {
+  GDPRMessage: string;
+  handleGDPRChange(input: boolean): void;
 }
 
-export const GDPR = ({ msg }: IProps) => {
-  return (
-    <>
-      <h2>GDPR works!</h2>
-      <p>{msg}</p>
-    </>
-  );
+interface IGDPRState {
+  isGDPRChecked: boolean;
+}
+
+export class GDPR extends React.Component<IGDPRProps, IGDPRState> {
+  constructor(props: IGDPRProps) {
+    super(props);
+
+    this.state = {
+      isGDPRChecked: false
+    }
+  }
+
+  handleInputChange = (event: any): void => {
+    const checkBoxChecked = event.target.checked;
+    let value: boolean;
+
+    checkBoxChecked ? value = true : value = false;
+
+    this.setState({
+      isGDPRChecked: value
+    });
+
+    this.props.handleGDPRChange(value);
+  }
+
+  render() {
+    const { GDPRMessage } = this.props;
+    const { isGDPRChecked } = this.state;
+
+    return (
+      <>
+        <div>
+          <input
+            type="checkbox"
+            name="GDPRCheckBox"
+            checked={isGDPRChecked}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <p>{GDPRMessage}</p>
+      </>
+    );
+  }
 }
