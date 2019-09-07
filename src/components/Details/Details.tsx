99f309b7a23b92dda32e9c4d077d733677181fdc
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-09-07 14:55:27 +0200

    added basic form styling to admin page

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -141,17 +141,53 @@ class Details extends React.Component< IAccordionProps, IBookingItem> {
   render() {
     return (
 			<div>
+				<h2 className="m-4">Handling booking with id: {this.state.booking_ID}</h2>
+				
+				<p className="mb-1">If you want to delete this booking, click the "delete button below"</p>
+				<p className="mb-4">If you want to edit this booking, make your changes and click the "save changes" button</p>
+				
 				<form onSubmit={(e) => this.handleSubmit(e)} className="update-form">
-					<div>
-						<input type="text" name="customer_ID" value={this.state.customer_ID} onChange={(e) => this.handleInputChange(e)} />
-						<input type="text" name="booking_ID" value={this.state.booking_ID} onChange={(e) => this.handleInputChange(e)} />
-						<input type="text" name="email" value={this.state.email} onChange={(e) => this.handleInputChange(e)} />
-						<input type="text" name="guests" value={this.state.guests} onChange={(e) => this.handleInputChange(e)} />
-						<input type="text" name="name" value={this.state.name} onChange={(e) => this.handleInputChange(e)} />
-						<input type="text" name="phone" value={this.state.phone} onChange={(e) => this.handleInputChange(e)} />
-						<input type="text" name="sitting" value={this.state.sitting} onChange={(e) => this.handleInputChange(e)} />
-						<button id="delete-button" onClick={() =>this.deleteBookingWithID(this.state.booking_ID)}>Delete</button>
-						<button type="submit">Submit</button>
+					<div className="form-row">
+						<div className="form-group d-flex flex-column col-12 col-md-6">
+							<label htmlFor="customerID">Customer ID</label>
+							<input readOnly value={this.state.customer_ID} id="customerID" />
+						</div>
+
+						<div className="form-group d-flex flex-column col-12 col-md-6">
+							<label htmlFor="bookingID">Booking ID</label>
+							<input readOnly value={this.state.booking_ID} id="bookingID"/>
+						</div>
+
+						<div className="form-group d-flex flex-column col-12 col-md-6">
+							<label htmlFor="email">Email</label>
+							<input type="text" name="email" id="email" value={this.state.email} onChange={(e) => this.handleInputChange(e)} />
+						</div>
+
+						<div className="form-group d-flex flex-column col-12 col-md-6">
+							<label htmlFor="guests">Number of guests</label>
+							<input type="text" name="guests" id="guests" value={this.state.guests} onChange={(e) => this.handleInputChange(e)} />
+						</div>
+
+						<div className="form-group d-flex flex-column col-12 col-md-4">
+							<label htmlFor="name">Name</label>
+							<input type="text" name="name" id="name" value={this.state.name} onChange={(e) => this.handleInputChange(e)} />
+						</div>
+
+						<div className="form-group d-flex flex-column col-12 col-md-4">
+							<label htmlFor="phone">Phone number</label>
+							<input type="text" name="phone" id="phone" value={this.state.phone} onChange={(e) => this.handleInputChange(e)} />
+						</div>
+
+						<div className="form-group d-flex flex-column col-12 col-md-4">
+							<label htmlFor="sitting">Sitting</label>
+							<input type="text" name="sitting" id="sitting" value={this.state.sitting} onChange={(e) => this.handleInputChange(e)} />
+						</div>
+
+						<div className="form-group col-12">
+							<button className="btn btn-outline-secondary" id="delete-button" onClick={() =>this.deleteBookingWithID(this.state.booking_ID)}>Delete booking</button>
+							<button className="btn btn-outline-secondary" type="submit">Save Changes</button>
+						</div>
+
 					</div>
 				</form>
 			</div>
