990900588e76a96d35edf47d37b772cc4620008f
Ulrica Bonnerstig <u.bonnerstig@gmail.com>
2019-09-11 14:31:07 +0200

    Add tests for AvailableTables component

--- src/components/AvailableTables/AvailableTables.test.tsx
+++ src/components/AvailableTables/AvailableTables.test.tsx
@@ -1,31 +1,45 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import AvailableTables from './AvailableTables';
+import AvailableTables, { IAvailableTablesProp } from './AvailableTables';
+import Booking from '../Booking/Booking';
 
 describe('AvailableTables Component', () => {
-	let props: any = {};
-  let AvailableTablesInstance: any;
+	let props: IAvailableTablesProp;
+	let AvailableTablesInstance: any;
+
+	beforeAll(() => {
+		props = {
+			dateTime: {
+				date: '2019-09-10',
+				time: '18:00:00'
+			},
+			config: {
+				sittingOne: '18:00:00',
+				sittingTwo: '21:00:00'
+			},
+			freeSeats: 0,
+			guests: 0,
+			handleTimeClick: jest.fn(),
+			handleSeatsClick: jest.fn()
+		}	
+	});
 
   beforeEach(() => {
     AvailableTablesInstance = mount<AvailableTables>(
       <AvailableTables {...props} />
-    );
+		);
   });
 
   afterEach(() => {
-    AvailableTablesInstance.unmount();
+		AvailableTablesInstance.unmount();
+	});
+
+	it('should render without crashing', () => {
+		shallow(<AvailableTables {...props} />);
 	});
 	
-	it('contains a date', () => {
-    // const test = shallow(<AvailableTables
-		// 	dateTime={this.state.dateTime}
-		// 	config={this.state.config}
-		// 	handleTimeClick={this.calculateFreeSeats}
-		// 	handleSeatsClick={this.handleSeatsClick}
-		// 	freeSeats={this.state.freeSeats}
-		// />);
-    // expect(test.containsMatchingElement(<h1>Booking works</h1>)).toEqual(true);
+	it('should contain set date', () => {
+		const test = shallow(<AvailableTables {...props} />);
+    expect(test.containsMatchingElement(<h3>2019-09-10</h3>)).toEqual(true);
   });
-
-
 });
