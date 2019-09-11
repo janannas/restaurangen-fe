26bc9944b2d5d1a3a1eeb735570fa648e49184af
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-09-11 12:32:55 +0200

    added form validation for admin details. using form text control component. removed update customer from api calls

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -3,15 +3,20 @@ import './Admin.css';
 import ApiCalls from '../../utils/ApiCalls';
 import { IBookingItem } from '../../interfaces/IBookingItem';
 import { Link } from 'react-router-dom';
-
-//const axios = require('axios');
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export interface IBookingState { 
   bookingInfo: IBookingItem[];
   on: boolean;
 }
 
-class Admin extends React.Component< {}, IBookingState > {
+interface IAdminProps {
+  history: {
+		push: any
+	}
+}
+
+class Admin extends React.Component< IAdminProps, IBookingState > {
   constructor(props: any) {
     super(props);
 
@@ -25,6 +30,16 @@ class Admin extends React.Component< {}, IBookingState > {
   componentDidMount() {
     this.getBookings();
   }
+
+      
+  deleteBookingWithID = (targetID: any) => {
+    if(window.confirm('Are you sure you want to delete this booking?')) {
+      new ApiCalls().deleteBooking(targetID).then((result: any) => {
+        this.props.history.push("/admin");
+        this.getBookings();
+      });
+    }
+	}
 
   public getBookings() {
     new ApiCalls()
@@ -66,7 +81,8 @@ class Admin extends React.Component< {}, IBookingState > {
             <span className="booking-info-span">{booking.name}</span>
             <span className="booking-info-span">{booking.guests}</span>
             <span className="booking-info-span">{booking.sitting}</span>
-            <Link to={`accordion/${booking.booking_ID}`}><button type="button" className="btn btn-outline-secondary">Handle booking</button></Link>
+            <Link to={`accordion/${booking.booking_ID}`}><button type="button" className="btn btn-outline-secondary">Manage booking</button></Link>
+            <button className="btn btn-outline-secondary" id="delete-button" onClick={() =>this.deleteBookingWithID(booking.booking_ID)}>Delete booking</button>
           </div>
           ))}
         </div>
--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Details.css' ;
+import { validate } from '../../utils/validation';
 import { IBookingItem } from '../../interfaces/IBookingItem';
 import ApiCalls from '../../utils/ApiCalls';
 import { IUpdateBooking } from '../../interfaces/IUpdateBooking';
 import { IUpdateCustomer } from '../../interfaces/IUpdateCustomer';
+import FormTextControl from "../FormTextControl/FormTextControl";
 
 const axios = require('axios');
 
@@ -18,24 +20,90 @@ export interface IAccordionProps {
 	},
 }
 
-class Details extends React.Component< IAccordionProps, IBookingItem> {
+interface IDetailsState {
+  formIsValid: boolean;
+
+  booking: IUpdateBooking;
+
+  formControls: {
+    name: {
+      value: string;
+      valid: boolean;
+      touched: boolean;
+      validationRules: {
+        isRequired: boolean;
+      };
+    };
+    email: {
+      value: string;
+      valid: boolean;
+      touched: boolean;
+      validationRules: {
+        isEmail: boolean;
+      };
+    };
+    phone: {
+      value: string;
+      valid: boolean;
+      touched: boolean;
+      validationRules: {
+        isNumber: boolean;
+        minLength: 3;
+      };
+    };
+  };
+  error: string;
+}
+
+class Details extends React.Component< IAccordionProps, IDetailsState> {
   constructor(props: any) {
 		super(props);
 		
 		this.state = {
-			booking_ID: 0,
-			customer_ID: 0,
-			email:'',
-			guests: 0,
-			name: '',
-			phone: '',
-			sitting: ''
+      formIsValid: false, //tracks the overall form validity
+
+			booking: {
+        booking_ID: 0,
+			  customer_ID: 0,
+		  	guests: 0,
+        sitting: ''
+      },
+
+      formControls: {
+        name: {
+          value: "",
+          valid: false,
+          touched: false,
+          validationRules: {
+            isRequired: true
+          }
+        },
+        email: {
+          value: "",
+          valid: false,
+          touched: false,
+          validationRules: {
+            isEmail: true
+          }
+        },
+        phone: {
+          value: "",
+          valid: false,
+          touched: false,
+          validationRules: {
+            isNumber: true,
+            minLength: 3
+          }
+        }
+      },
+
+      error: ""
 		};
   
 	}
 
 	componentDidMount() {
-		this.getBooking();
+    this.getBooking();
 	}
 	
 	public getBooking() {
@@ -43,13 +111,13 @@ class Details extends React.Component< IAccordionProps, IBookingItem> {
     .getAllBookings().then((result: any) => {
 			var tempArray: IBookingItem[] = [];
 			var mapArray: IBookingItem = {
-				booking_ID: 0,
-				customer_ID: 0,
-				email:'',
-				guests: 0,
-				name: '',
-				phone: '',
-				sitting: ''
+        booking_ID: 0,
+        customer_ID: 0,
+        email:'',
+        guests: 0,
+        name: '',
+        phone: '',
+        sitting: ''
 			};
 
 			tempArray = result.data;
@@ -60,37 +128,79 @@ class Details extends React.Component< IAccordionProps, IBookingItem> {
 			});
 
 			this.setState({
-				booking_ID: mapArray.booking_ID,
-				customer_ID: mapArray.customer_ID,
-				email: mapArray.email,
-				guests: mapArray.guests,
-				name: mapArray.name,
-				phone: mapArray.phone,
-				sitting: mapArray.sitting
+				booking: {
+          booking_ID: mapArray.booking_ID,
+				  customer_ID: mapArray.customer_ID,
+			  	guests: mapArray.guests,
+          sitting: mapArray.sitting
+        },
+        formControls: {
+          name: {
+            value: mapArray.name,
+            valid: false,
+            touched: false,
+            validationRules: {
+              isRequired: true
+            }
+          },
+          email: {
+            value: mapArray.email,
+            valid: false,
+            touched: false,
+            validationRules: {
+              isEmail: true
+            }
+          },
+          phone: {
+            value: mapArray.phone,
+            valid: false,
+            touched: false,
+            validationRules: {
+              isNumber: true,
+              minLength: 3
+            }
+          }
+        },
+
 			});
 	})
 	.catch(error => {
-			console.log(error);
+		console.log(error);
 	});
-	}
-    
-  deleteBookingWithID = (targetID: any) => {
-    if(window.confirm('Are you sure you want to delete this booking?')) {
-      new ApiCalls().deleteBooking(targetID).then((result: any) => {
-				this.props.history.push("/admin");
-      });
-    }
 	}
 
 	handleInputChange = async (event: any) =>{
-		const target = event.target;
-		const value = target.value;
+		// const target = event.target;
+		// const value = target.value;
 
-		await this.setState({
-			[target.name]: value
-		} as any);
+		// await this.setState({
+		// 	[target.name]: value
+    // } as any);
+    
+    const name: keyof IDetailsState["formControls"] = event.target.name;
+    const value = event.target.value;
 
-		console.log(this.state);
+    const updatedControls = {
+      ...this.state.formControls
+    };
+
+    let updatedControl = updatedControls[name];
+
+    updatedControl.value = value;
+
+    // Don't want to validate the first time user touches input-field
+    if (updatedControl.touched) {
+      updatedControl.valid = validate(value, updatedControl.validationRules);
+    }
+
+    // Submit button gets activated soon as form i valid
+    let formIsValid: boolean = this.validateForm();
+
+    this.setState({
+      formIsValid: formIsValid,
+      formControls: updatedControls
+    });
+
 	}
 
 	updateCustomer = (customerToUpdate: IUpdateCustomer) => {
@@ -101,52 +211,149 @@ class Details extends React.Component< IAccordionProps, IBookingItem> {
 			})
 	}
 
-	updateBooking = (bookingToUpdate: IUpdateBooking) => {
-		axios
-    	.put('http://localhost/admin/update-booking.php/', bookingToUpdate)
-    	.then(() => {
-				this.props.history.push("/admin");
-			})
-	}
+	// updateBooking = (bookingToUpdate: IUpdateBooking) => {
+	// 	axios
+  //   	.put('http://localhost/admin/update-booking.php/', bookingToUpdate)
+  //   	.then(() => {
+	// 			this.props.history.push("/admin");
+	// 		})
+  // }
 
+  validateForm = () => {
+    const updatedControls = {
+      ...this.state.formControls
+    };
+
+    // temp object to set state
+    let formIsValid: boolean = true;
+    let inputIdentifier: keyof IDetailsState["formControls"];
+
+    // Checking that all controls are valid
+    for (inputIdentifier in updatedControls) {
+      formIsValid = updatedControls[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({
+      formIsValid: formIsValid
+    });
+
+    return formIsValid;
+  }
+  
+  validateControl = (event: any) => {
+    const name: keyof IDetailsState["formControls"] = event.target.name;
+
+    const updatedControls = {
+      ...this.state.formControls
+    };
+
+    let updatedControl = updatedControls[name];
+
+    updatedControl.touched = true;
+    updatedControl.valid = validate(updatedControl.value, updatedControl.validationRules);
+
+    this.setState({
+      formControls: updatedControls
+    });
+  }
+  
+  triggerAllValidation = (event: any) => {
+    this.validateControl(event);
+    this.validateForm();
+  }
 	
 	handleSubmit = (event: any) => {
-		event.preventDefault();
+	 	event.preventDefault();
 
-		var bookingToUpdate: IUpdateBooking;
-		var customerToUpdate: IUpdateCustomer;
+	 	//var bookingToUpdate: IUpdateBooking;
+	 	var customerToUpdate: IUpdateCustomer;
 
-		customerToUpdate = {
-			customer_ID: this.state.customer_ID,
-			name: this.state.name,
-			email: this.state.email,
-			phone: this.state.phone
-		}
+	 	customerToUpdate = {
+	 		customer_ID: this.state.booking.customer_ID,
+	 		name: this.state.formControls.name.value,
+	 		email: this.state.formControls.email.value,
+	 		phone: this.state.formControls.phone.value
+     }
+     console.log(this.state.booking.customer_ID);
 
-		bookingToUpdate = {
-		booking_ID: this.state.booking_ID,
-		customer_ID: this.state.customer_ID,
-		guests: this.state.guests,
-		sitting: this.state.sitting
-		}
+	 	// bookingToUpdate = {
+	  // 	booking_ID: this.state.booking.booking_ID,
+	 	//   customer_ID: this.state.booking.customer_ID,
+	 	//   guests: this.state.booking.guests,
+	  // 	sitting: this.state.booking.sitting
+	 	// }
 
-		if(window.confirm('Are you sure you want to save this update?')) {
-			this.updateCustomer(customerToUpdate);
-			this.updateBooking(bookingToUpdate);
-		}
+	 	if(window.confirm('Are you sure you want to save this update?')) {
+	 		this.updateCustomer(customerToUpdate);
+	 		// this.updateBooking(bookingToUpdate);
+	 	}
 		
 	}
   
   render() {
     return (
 			<div className="details-wrapper">
-				<h3 className="m-4 text-center">Handling booking with id: {this.state.booking_ID}</h3>
+				<h3 className="m-4 text-center">Handling booking with id: {this.state.booking.booking_ID}</h3>
 				
 				<p className="mb-1 text-center">If you want to delete this booking, click the "delete" button below</p>
 				<p className="mb-4 text-center">If you want to edit this booking, make your changes and click the "save changes" button</p>
 				
 				<form onSubmit={(e) => this.handleSubmit(e)} className="details-form pt-2">
-					<div className="form-row">
+          <label htmlFor="customer_ID">Customer ID: </label>
+          <input disabled id="customer_ID" value={this.state.booking.customer_ID}></input>
+          <label htmlFor="booking_ID">Booking ID: </label>
+          <input disabled id="booking_ID" value={this.state.booking.booking_ID}></input>
+          <label htmlFor="sitting">Sitting: </label>
+          <input disabled id="sitting" value={this.state.booking.sitting}></input>
+          <label htmlFor="guests">Number fo guests: </label>
+          <input disabled id="guests" value={this.state.booking.guests}></input>
+          
+
+        <FormTextControl
+          name="name"
+          htmlFor="name"
+          onChange={this.handleInputChange}
+          onBlur={this.triggerAllValidation}
+          value={this.state.formControls.name.value}
+          id="name"
+          label="Name: "
+          touched={this.state.formControls.name.touched ? 1 : 0}
+          valid={this.state.formControls.name.valid ? 1 : 0}
+          error={"Field is required"}
+        />
+
+        <FormTextControl
+          name="email"
+          htmlFor="email"
+          onChange={this.handleInputChange}
+          onBlur={this.triggerAllValidation}
+          value={this.state.formControls.email.value}
+          id="email"
+          label="Email: "
+          touched={this.state.formControls.email.touched ? 1 : 0}
+          valid={this.state.formControls.email.valid ? 1 : 0}
+          error={"Please enter a valid email-address"}
+        />
+
+        <FormTextControl
+          name="phone"
+          htmlFor="phone"
+          onChange={this.handleInputChange}
+          onBlur={this.triggerAllValidation}
+          value={this.state.formControls.phone.value}
+          id="phone"
+          label="Phone Number: "
+          touched={this.state.formControls.phone.touched ? 1 : 0}
+          valid={this.state.formControls.phone.valid ? 1 : 0}
+          error={"Please enter at least three digits"}
+        />
+
+        <button type="submit" disabled={!this.state.formIsValid}>Submit changes</button>
+
+
+
+
+					{/* <div className="form-row">
 						<div className="form-group d-flex flex-column col-12 col-md-4">
 							<label htmlFor="customerID">Customer ID</label>
 							<input disabled value={this.state.customer_ID} id="customerID" />
@@ -188,7 +395,7 @@ class Details extends React.Component< IAccordionProps, IBookingItem> {
 							<button className="btn btn-outline-secondary ml-2" type="submit">Save Changes</button>
 						</div>
 
-					</div>
+					</div> */}
 				</form>
 			</div>
 		);
